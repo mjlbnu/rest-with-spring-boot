8e187f758f32013781e40a8195d054100167b320
mjlbnu <mjlbnu@gmail.com>
2024-02-12 22:10:11 -0300

    Removing warnings and annotation repository

--- src/main/java/br/com/programadorbr/repositories/PersonRepository.java
+++ src/main/java/br/com/programadorbr/repositories/PersonRepository.java
@@ -2,7 +2,5 @@ package br.com.programadorbr.repositories;
 
 import br.com.programadorbr.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}

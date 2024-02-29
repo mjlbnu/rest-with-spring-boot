f576168641e39b209139e8133031d17ce53e9973
mjlbnu <mjlbnu@gmail.com>
2024-02-28 22:34:33 -0300

    Migrations com Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person`
+(`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(2,'Lost Island','John','Male','Locke updated'),
+(3,'Miami','Dexter','Male','Morgan'),
+(8,'Blumenau','Pink','Female','Morgan'),
+(11,'Chicago','Eliot','Male','Robot'),
+(12,'Blumenau','Pink','Female','Morgan');

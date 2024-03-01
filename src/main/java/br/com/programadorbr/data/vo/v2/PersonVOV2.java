4dc049033ed9963f3bc284fc53f33c854f1232c7
mjlbnu <mjlbnu@gmail.com>
2024-02-29 21:49:47 -0300

    Custom Json Serialization

--- src/main/java/br/com/programadorbr/data/vo/v2/PersonVOV2.java
+++ src/main/java/br/com/programadorbr/data/vo/v2/PersonVOV2.java
@@ -8,15 +8,10 @@ public class PersonVOV2 implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
-
     private String firstName;
-
     private String lastName;
-
     private String address;
-
     private String gender;
-
     private Date birthDay;
 
     public PersonVOV2() {

ba0de6390380d85e23700976563d6f9cf07540a5
mjlbnu <mjlbnu@gmail.com>
2024-06-20 22:45:34 -0300

    Validando a geração da swagger ui page com test containers e rest assured

--- src/test/java/br/com/programadorbr/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ src/test/java/br/com/programadorbr/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -13,7 +13,7 @@ import java.util.stream.Stream;
 
 @ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public class AbstractIntegrationTest {
-    public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.29");
 
         private static void startContainers() {
